cddc0451800f56083a1af93ced365afd96da2fe2
Shadowbear <lenniesi@web.de>
2025-04-04 21:16:46 +0200

    Cleanup

--- Models/Address.cs
+++ Models/Address.cs
@@ -6,28 +6,33 @@ namespace AddressList.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "FileID ist ein Pflichtfeld")]
         public int FileID { get; set; }
+        
+        [Required(ErrorMessage = "Aktenzeichen ist ein Pflichtfeld")]
         public string Aktenzeichen { get; set; }
+        
+        public string Rechtsform { get; set; } = string.Empty;
+        public string Anrede { get; set; } = string.Empty;
+        public string Titel { get; set; } = string.Empty;
 
-        public string Rechtsform { get; set; }
-
-        public string Anrede { get; set; }
-
-        public string Titel { get; set; }
-
+        [Required(ErrorMessage = "Vorname ist ein Pflichtfeld")]        
         public string Vorname { get; set; }
+
+        [Required(ErrorMessage = "Nachname ist ein Pflichtfeld")]
         public string Nachname { get; set; }
-        public string Straße { get; set; }
-        public string Hausnummer { get; set; }
-        public string PLZ { get; set; }
-        public string Ort { get; set; }
-        public string Land { get; set; }
 
-        public string Import { get; set; }
-        public string Datenquelle { get; set; }
-
+        public string Straße { get; set; } = string.Empty;
+        public string Hausnummer { get; set; } = string.Empty;
+        public string PLZ { get; set; } = string.Empty;
+        public string Ort { get; set; } = string.Empty;
+        public string Land { get; set; } = string.Empty;
+        public string Import { get; set; } = string.Empty;
+        public string Datenquelle { get; set; } = string.Empty;
         public bool AktuelleAnschrift { get; set; }
 
+        [Required(ErrorMessage = "Status ist ein Pflichtfeld")]
         public string AddressStatus { get; set; }
     }
 }
